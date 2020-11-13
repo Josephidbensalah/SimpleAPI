31d1fe12adea2ce83f035a44de5a0315bbf8c33c
Josephidbensalah <joseph.idbensalah@gmail.com>
2020-11-13 17:43:58 +0100

    Initial Commit

---
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id){
+            return "Joseph bens";
+        }
+    }
+}
